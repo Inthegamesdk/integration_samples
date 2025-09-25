2451c2cf06fac7483a520ea4ba2b32429138f740
roman.havdulskyi <romeogavdulskiy@gmail.com>
2025-09-25 13:19:25 +0300

    ITG: provided Bitmovin demo

--- app/src/main/java/com/syncedapps/inthegametvexample/PlaybackPhoneActivity.kt
+++ app/src/main/java/com/syncedapps/inthegametvexample/PlaybackPhoneActivity.kt
@@ -1,43 +1,66 @@
 package com.syncedapps.inthegametvexample
 
 import android.annotation.SuppressLint
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import android.view.KeyEvent
 import android.view.WindowInsets
 import android.view.WindowManager
-import androidx.annotation.OptIn
-import androidx.fragment.app.FragmentActivity
-import androidx.media3.common.MediaItem
-import androidx.media3.common.util.UnstableApi
-import androidx.media3.datasource.DefaultDataSource
-import androidx.media3.datasource.DefaultHttpDataSource
-import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.exoplayer.hls.HlsMediaSource
-import androidx.media3.exoplayer.source.MediaSource
-import androidx.media3.exoplayer.source.ProgressiveMediaSource
-import androidx.media3.ui.PlayerView
-import com.syncedapps.inthegametvexample.databinding.ActivityPhonePlaybackBinding
-import android.view.ViewGroup
-import java.util.*
-
-import com.syncedapps.inthegametv.integration.ITGMedia3PlayerAdapter
-import com.syncedapps.inthegametv.integration.ITGPlaybackComponent
 import androidx.activity.OnBackPressedCallback
-import android.view.KeyEvent
+import androidx.fragment.app.FragmentActivity
+import com.bitmovin.analytics.api.AnalyticsConfig
+import com.bitmovin.analytics.api.SourceMetadata
+import com.bitmovin.player.PlayerView
+import com.bitmovin.player.api.Player
+import com.bitmovin.player.api.analytics.AnalyticsPlayerConfig
+import com.bitmovin.player.api.analytics.AnalyticsSourceConfig
+import com.bitmovin.player.api.source.Source
+import com.bitmovin.player.api.source.SourceConfig
+import com.bitmovin.player.api.source.SourceType
+import com.syncedapps.inthegametv.integration.ITGBitmovinPlayerAdapter
+import com.syncedapps.inthegametv.integration.ITGPlaybackComponent
+import com.syncedapps.inthegametvexample.databinding.ActivityPhonePlaybackBinding
+import java.util.*
 
 class PlaybackPhoneActivity : FragmentActivity() {
 
     private lateinit var binding: ActivityPhonePlaybackBinding
-    private var player: ExoPlayer? = null
     private var playbackPosition: Long = 0L
     private var playWhenReady: Boolean = true
-    private var videoView : PlayerView? = null
 
     private var mITGComponent: ITGPlaybackComponent? = null
-    private var mITGPlayerAdapter: ITGMedia3PlayerAdapter? = null
+    private var mITGPlayerAdapter: ITGBitmovinPlayerAdapter? = null
 
+    private var analyticsLicenseKey = "fa295408-0e32-4aa9-867d-57cc40f4130d"
+    private lateinit var playerView: PlayerView
+
+
+    override fun onStart() {
+        super.onStart()
+        playerView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        playerView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        playerView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        playerView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // ITG: release player
+        mITGPlayerAdapter?.onPlayerReleased()
+        playerView.onDestroy()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,11 +71,38 @@ class PlaybackPhoneActivity : FragmentActivity() {
         setContentView(binding.root)
         setupFullscreenMode()
 
-        //add video view
-        videoView = buildVideoView()
+        val analyticsConfig = AnalyticsConfig(
+            licenseKey = analyticsLicenseKey,
+        )
+        val player = Player(
+            context = this,
+            analyticsConfig = AnalyticsPlayerConfig.Enabled(analyticsConfig),
+        )
+        playerView = binding.playerView
+        playerView.player = player
 
-        startVideo()
+        val streamTitle = "ITG BITMOVIN DEMO"
+        val source = Source(
+            SourceConfig(
+                url = Const.VIDEO_URL,
+                type = SourceType.Progressive,
+                title = streamTitle,
+            ),
+            AnalyticsSourceConfig.Enabled(
+                SourceMetadata(
+                    videoId = "android-wizard-Art_of_Motion-1758794536553",
+                    title = streamTitle,
+                )
+            ),
+        )
 
+        player.load(source)
+
+        // ITG: init
+        initITG(player, savedInstanceState)
+    }
+
+    private fun initITG(player: Player, savedInstanceState: Bundle?) {
         // Replace 'your_account_id' and 'your_channel_slug' with actual values
         val accountId = "68650da0324217d506bcc2d4"
         val channelSlug = "samplechannel"
@@ -63,9 +113,11 @@ class PlaybackPhoneActivity : FragmentActivity() {
 
 
         // Set up the ITGMedia3PlayerAdapter with your player view
-        val adapter = ITGMedia3PlayerAdapter(playerView = videoView)
+        val adapter = ITGBitmovinPlayerAdapter(playerView = playerView)
         mITGPlayerAdapter = adapter
 
+        // provide ITG with the player instance
+        adapter.onPlayerReady(player)
 
         // Initialize the ITG component with necessary parameters
         mITGComponent?.init(
@@ -81,15 +133,56 @@ class PlaybackPhoneActivity : FragmentActivity() {
         // Add the ITG component to your view hierarchy
         binding.outerContainer.addView(mITGComponent, 0)
 
+        // ITG: handle back press
         onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
+                // ITG: make sure ITG does not consume this back press
                 if (mITGComponent == null || mITGComponent?.handleBackPressIfNeeded() == false) {
                     // Implement your own back press action here
                     finish()
                 }
             }
         })
+    }
 
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putLong("playbackPosition", playbackPosition)
+        outState.putBoolean("playWhenReady", playWhenReady)
+
+        // ITG: saving the state of the SDK
+        mITGComponent?.onSaveInstanceState(outState)
+    }
+
+    // ITG: handle key events
+    @SuppressLint("RestrictedApi")
+    override fun dispatchKeyEvent(event: KeyEvent?): Boolean {
+        // ITG: make sure ITG doesn't consume the event
+        if (mITGComponent?.itgOverlayView?.isKeyEventConsumable(event) == true)
+            return super.dispatchKeyEvent(event)
+        // ... rest of your dispatchKeyEvent code
+        return super.dispatchKeyEvent(event)
+    }
+
+
+    // ITG: handle key events
+    override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
+        // ITG: make sure ITG doesn't consume the event
+        if (mITGComponent?.itgOverlayView?.isKeyEventConsumable(event) == true)
+            return super.onKeyUp(keyCode, event)
+        // ... rest of your onKeyUp code
+        return super.onKeyUp(keyCode, event)
+    }
+
+
+    // ITG: handle key events
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        // ITG: make sure ITG doesn't consume the event
+        if (mITGComponent?.itgOverlayView?.isKeyEventConsumable(event) == true)
+            return super.onKeyDown(keyCode, event)
+        // ... rest of your onKeyDown code
+        return super.onKeyDown(keyCode, event)
     }
 
     private fun restorePlaybackStateIfAny(savedInstanceState: Bundle?) {
@@ -111,130 +204,4 @@ class PlaybackPhoneActivity : FragmentActivity() {
         }
     }
 
-    private fun startVideo() {
-        prepareMediaForPlaying(Uri.parse(Const.VIDEO_URL))
-        player?.playWhenReady = playWhenReady
-        player?.seekTo(0, playbackPosition)
-        player?.prepare()
-    }
-
-    @OptIn(UnstableApi::class)
-    private fun prepareMediaForPlaying(mediaSourceUri: Uri) {
-        val upstreamDataSourceFactory = DefaultHttpDataSource.Factory()
-            .setAllowCrossProtocolRedirects(true)
-
-        val defaultDataSourceFactory =
-            DefaultDataSource.Factory(this, upstreamDataSourceFactory)
-
-        defaultDataSourceFactory.createDataSource()
-
-        val mediaSource: MediaSource =
-            if (mediaSourceUri.lastPathSegment?.endsWith(".m3u8") == true) {
-                HlsMediaSource.Factory(defaultDataSourceFactory)
-                    .createMediaSource(
-                        MediaItem.fromUri(mediaSourceUri)
-                    )
-            } else {
-                ProgressiveMediaSource.Factory(defaultDataSourceFactory)
-                    .createMediaSource(
-                        MediaItem.fromUri(mediaSourceUri)
-                    )
-            }
-        player?.setMediaSource(mediaSource)
-    }
-
-    @SuppressLint("InflateParams")
-    private fun buildVideoView(): PlayerView {
-        val videoView = layoutInflater.inflate(R.layout.styled_player_view, null, false) as PlayerView
-        videoView.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
-        return videoView    }
-
-    @OptIn(UnstableApi::class)
-    private fun initializePlayer() {
-        val player = ExoPlayer.Builder(this)
-            .setSeekBackIncrementMs(SEEK_INCREMENT)
-            .setSeekForwardIncrementMs(SEEK_INCREMENT)
-            .build()
-
-        // Notify the ITGPlayerAdapter that the player is ready
-        mITGPlayerAdapter?.onPlayerReady(player)
-
-        videoView?.player = player
-        this.player = player
-    }
-
-    private fun releasePlayer() {
-        Log.d(this.javaClass.simpleName, "releasePlayer")
-        player?.let { exoPlayer ->
-            playbackPosition = exoPlayer.currentPosition
-            playWhenReady = exoPlayer.playWhenReady
-            videoView?.player = null
-            exoPlayer.release()
-
-            // Notify the ITGPlayerAdapter that the player has been released
-            mITGPlayerAdapter?.onPlayerReleased()
-
-        }
-        player = null
-    }
-
-    override fun onResume() {
-        super.onResume()
-        if ((Build.VERSION.SDK_INT <= 23 || player == null)) {
-            initializePlayer()
-            startVideo()
-        }
-    }
-
-    override fun onStop() {
-        if (Build.VERSION.SDK_INT > 23) {
-            releasePlayer()
-        }
-        super.onStop()
-    }
-
-    public override fun onPause() {
-        super.onPause()
-        if (Build.VERSION.SDK_INT <= 23) {
-            releasePlayer()
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putLong("playbackPosition", playbackPosition)
-        outState.putBoolean("playWhenReady", playWhenReady)
-
-        // Saving the state of the SDK
-        mITGComponent?.onSaveInstanceState(outState)
-    }
-
-    @SuppressLint("RestrictedApi")
-    override fun dispatchKeyEvent(event: KeyEvent?): Boolean {
-        if (mITGComponent?.itgOverlayView?.isKeyEventConsumable(event) == true)
-            return super.dispatchKeyEvent(event)
-        // ... rest of your dispatchKeyEvent code
-        return super.dispatchKeyEvent(event)
-    }
-
-
-    override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
-        if (mITGComponent?.itgOverlayView?.isKeyEventConsumable(event) == true)
-            return super.onKeyUp(keyCode, event)
-        // ... rest of your onKeyUp code
-        return super.onKeyUp(keyCode, event)
-    }
-
-
-    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        if (mITGComponent?.itgOverlayView?.isKeyEventConsumable(event) == true)
-            return super.onKeyDown(keyCode, event)
-        // ... rest of your onKeyDown code
-        return super.onKeyDown(keyCode, event)
-    }
-
-
-    companion object {
-        private const val SEEK_INCREMENT = 10_000L
-    }
 }
