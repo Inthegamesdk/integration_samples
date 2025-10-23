16fe5b5efd877a559e649ad7ffeb56fded43965d
roman.havdulskyi <romeogavdulskiy@gmail.com>
2025-10-23 15:30:03 +0300

    React Native Expo: ITG 2.6.31 Android

--- App.tsx
+++ App.tsx
@@ -1,18 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ExpoItgVideoView } from 'expo-itg-video';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+  return <ExpoItgVideoView style={{ flex: 1, backgroundColor: 'purple' }} />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ expo-itg-video/src/index.ts
@@ -0,0 +1,3 @@
+// Reexport the native module. On web, it will be resolved to ExpoItgVideoModule.web.ts
+// and on native platforms to ExpoItgVideoModule.ts
+export { default as ExpoItgVideoView } from './ExpoItgVideoView';
