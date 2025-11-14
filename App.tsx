1c378070ed31a0bc1dfc59a11c5a233677524a7c
roman.havdulskyi <romeogavdulskiy@gmail.com>
2025-11-14 12:57:04 +0200

    Android build fixes

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { ExpoItgVideoView } from 'expo-itg-video';
 
 export default function App() {
-  return <ExpoItgVideoView style={{ flex: 1, backgroundColor: 'purple' }} />;
+  return <ExpoItgVideoView style={{ flex: 1, backgroundColor: 'black' }} />;
 }
