d0cc9f90d2143f73d84027ac8d9179107bc61c14
IlyaK1 <ilya.k@pettersonapps.com>
2025-12-01 14:57:13 +0200

    Initial commit

---
+++ ITGSwiftUISampleFastIntegration/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by ilya khymych on 26.11.2025.
+//
+
+import SwiftUI
+import AVKit
+#if os(tvOS)
+import Inthegametv
+#else
+import InthegametviOS
+#endif
+import ITGPlayerViewControllerSwiftUI
+import ITGPlayerViewController
+
+struct PlayerViewModel {
+    
+    let avplayer: AVPlayer
+    let videoView: AnyView
+    
+    init(_ videoUrl: URL) {
+        self.avplayer = AVPlayer(url: videoUrl)
+        self.videoView = AnyView(VideoPlayer(player: avplayer).ignoresSafeArea())
+    }
+
+}
+
+struct ContentView: View {
+    
+    enum FocusableItem: Hashable {
+        case itgOverlay
+        case player
+    }
+
+    @State var playerViewModel: PlayerViewModel = PlayerViewModel(URL(string: "https://assets.inthegame.io/admin-assets/black_screen_with_timer.mp4")!)
+    @State var channelSlug = "samplechannel"
+    @State var accountId = "68650da0324217d506bcc2d4"
+    @State var env = ITGEnvironment(envName: "v2-3")
+    
+    var body: some View {
+        ITGPlayerViewControllerSwiftUI(channelSlug: channelSlug, accountId: accountId, environment: env, enableLogs: true, playerAdapter: ITGAVPlayerAdapter(playerViewModel.avplayer, playerView: UIHostingController(rootView: playerViewModel.videoView).view))
+            .ignoresSafeArea()
+            .onAppear(perform: {
+                playerViewModel.avplayer.play()
+            })
+    }
+    
+}
+
+#Preview {
+    ContentView()
+}
+
---
+++ ITGSwiftUISampleFastIntegration/ITGSwiftUISampleFastIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITGSwiftUISampleFastIntegrationApp.swift
+//  ITGSwiftUISampleFastIntegration
+//
+//  Created by ilya khymych on 27.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct ITGSwiftUISampleFastIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
