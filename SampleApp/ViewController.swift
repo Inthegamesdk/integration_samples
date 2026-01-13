b8a7e5cdd95e806c22bb01444f5dd1cc6b96f521
IlyaK1 <ilya.k@pettersonapps.com>
2026-01-13 16:22:58 +0200

    Initial commit

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by ilya khymych on 25.03.2025.
+//
+
+import UIKit
+import AVKit
+import ITGPlayerViewController
+#if os(tvOS)
+import Inthegametv
+#else
+import InthegametviOS
+#endif
+
+class ViewController: UIViewController {
+    
+    let mediaUrl = "https://assets.inthegame.io/admin-assets/black_screen_with_timer.mp4"
+    let channelSlug: String = "samplechannel"
+    let accountId: String = "68650da0324217d506bcc2d4"
+    var itgPlayerController: ITGPlayerViewController!
+        
+    @IBAction func openChannelAction(_ sender: Any) {
+      
+        let player = AVPlayer(url: URL(string: mediaUrl)!)
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+        
+        let playerAdapter = ITGAVPlayerAdapter(player, playerViewController: playerViewController)
+        itgPlayerController = ITGPlayerViewController(channelSlug: channelSlug, accountId: accountId, playerAdapter: playerAdapter)
+        itgPlayerController.shouldPlayChannelVideo = false
+#if os(iOS)
+        itgPlayerController.closeButtonVisibilityMode = .hidden
+#endif
+        view.addSubview(itgPlayerController.view)
+        itgPlayerController.view.constraintsFillSuperview()
+        player.play()
+        
+    }
+    
+}
+
