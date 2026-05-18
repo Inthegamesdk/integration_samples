0032965ad4cb108382c37413caba1659d13fa4dd
roman.havdulskyi <romeogavdulskiy@gmail.com>
2026-05-18 14:46:30 +0300

    Added Sky DASH MT demo

--- app/src/main/java/com/syncedapps/inthegametvexample/Const.kt
+++ app/src/main/java/com/syncedapps/inthegametvexample/Const.kt
@@ -2,5 +2,5 @@ package com.syncedapps.inthegametvexample
 
 
 object Const {
-    const val MEDITAILOR_URL = "https://d37963fd1a374034af5ccda89ee59e85.mediatailor.us-east-1.amazonaws.com/v1/session/dffceb859a31f14d0ed059ef6b4e2e4d850e60b1/livh_1091/1091_E_ENTERTAINMENT_HD_HLS/master.m3u8"
+    const val MEDITAILOR_URL = "https://491f7f245b30483e865404117bfc6870.mediatailor.us-east-1.amazonaws.com/v1/session/dffceb859a31f14d0ed059ef6b4e2e4d850e60b1/live_1091_itg/1091_E_ENTERTAINMENT_HD_DASH/manifest.mpd"
 }
