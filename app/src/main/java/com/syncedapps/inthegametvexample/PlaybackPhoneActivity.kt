cf97a6849ad81b17ef1d556401082e18b299ce10
roman.havdulskyi <romeogavdulskiy@gmail.com>
2026-06-24 10:35:04 +0300

    2.7.0.29

--- app/src/main/java/com/syncedapps/inthegametvexample/PlaybackPhoneActivity.kt
+++ app/src/main/java/com/syncedapps/inthegametvexample/PlaybackPhoneActivity.kt
@@ -78,7 +78,6 @@ class PlaybackPhoneActivity : FragmentActivity() {
 
             accountId = accountId, //mandatory: your ITG accountId
             channelSlug = channelSlug, //mandatory: your channelId on our admin panel
-            itgEnvironment = ITGEnvironment.dev,
             enableLogs = true
         )
 
